bf07dae3bfcac56d78bdd9ed7511a5a0c9c5f5e3
BuildTools <unconfigured@null.spigotmc.org>
2020-08-08 14:38:07 +0900

    1.0-dc10

---
+++ lectures/lecture-dailyCoding/dc03.js
@@ -0,0 +1,12 @@
+// dc03.js - day & night
+
+
+function getNight() {
+  document.querySelector('body').style.backgroundColor = 'black';
+  document.querySelector('body').style.color = 'white';
+}
+
+function getDay() {
+  document.querySelector('body').style.backgroundColor = 'white';
+  document.querySelector('body').style.color = 'black';
+}
