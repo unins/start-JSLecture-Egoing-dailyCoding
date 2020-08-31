b7480f89cd2dc2e5448ee8b0bcf8852ff054978e
unins <blacklego@naver.com>
2020-08-31 17:34:28 +0900

    js-home-contact

--- portfolio-clone-coding/main.js
+++ portfolio-clone-coding/main.js
@@ -12,13 +12,23 @@ document.addEventListener('scroll', () => {
 });
 
 // Handle scrolling when tapping on the navbar menu
-const navbarMenu = document.querySelector('.navbar__menu')
+const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
     const target = event.target;
     const link = target.dataset.link;
     if (link == null) {
         return;
     }
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
+    scrollIntoView(link);
 })
+
+// Handle click on "contact me" button on home
+const contactBtn = document.querySelector('.home__contact');
+contactBtn.addEventListener('click', () => {
+    scrollIntoView('#contact');
+})
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+}
